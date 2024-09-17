619ee385ea11652bad26379b1f2ea11ef2b80859
Peer Mohammad <peeru548@gmail.com>
2024-09-17 10:01:48 +0530

    fix :bug fixed

--- dist/server.js
+++ dist/server.js
@@ -18,7 +18,7 @@ dotenv_1.default.config();
 app.use((0, morgan_1.default)("dev"));
 // Setting Cors 
 app.use((0, cors_1.default)({
-    origin: "https://blog-management-system-omega.vercel.app",
+    origin: ["https://blog-management-system-omega.vercel.app", "http://localhost:5000"],
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
