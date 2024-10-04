f24eea25a31a8aa238254a91ad32b048b2602d8b
Peer Mohammad <peeru548@gmail.com>
2024-10-04 16:29:49 +0530

    fix : changes

--- src/server.ts
+++ src/server.ts
@@ -45,4 +45,10 @@ app.listen(PORT,()=>{
   console.log("Server is running : http://localhost:4000")
 })
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 export default app
