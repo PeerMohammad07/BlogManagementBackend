8468ca30f2a71f4f03aa2c2ba3d8e6b42cf72f8c
Peer Mohammad <peeru548@gmail.com>
2024-10-04 16:47:23 +0530

    fix : changes

--- src/server.ts
+++ src/server.ts
@@ -17,10 +17,20 @@ app.use(morgan("dev"))
 
 // Setting Cors 
 app.use(cors({
-  origin: "https://blog-management-client",
+  origin: "https://blog-management-client.vercel.app",
   credentials: true,
 }));
 
+
+app.use((req, res, next) => {
+  res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  });
+
+  next();
+});
 
 
 app.use(cookieParser())
